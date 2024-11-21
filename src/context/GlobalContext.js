9263ff2f1a346caab2f9473da17009080f30c380
Jhone Jhee Patacsil <jhonejheeadrian.patacsil@g.batstate-u.edu.ph>
2024-11-21 15:55:38 +0000

    added function to add groups

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -4,22 +4,7 @@ export const GlobalContext = createContext({});
 
 export const GlobalProvider = ({ children }) => {
     const [dark, setDark] = useState(true)
-    const [soundBoard, setSoundBoard] = useState([
-        {
-            "name": "Group1",
-            "sounds": [
-                { "name": "Applaud", "path": "", "volume": 80, "muted": true },
-                // { "name": "Applaud2", "path": "", "volume": 65, "muted": false }
-            ]
-        },
-        {
-            "name": "Group2",
-            "sounds": [
-                // { "name": "Clap", "path": "", "volume": 55, "muted": true },
-                // { "name": "Clap2", "path": "", "volume": 100, "muted": false }
-            ]
-        }
-    ]);
+    const [soundBoard, setSoundBoard] = useState([]);
     const [master, setMaster] = useState({
         "name": "master",
         "path": "",
--- src/layout/MenuBar.jsx
+++ src/layout/MenuBar.jsx
@@ -1,11 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from "components/ui/menubar";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter, DialogTrigger } from "components/ui/dialog"
 import { Switch } from "components/ui/switch";
 import { Sun, Moon } from 'lucide-react';
 import { GlobalContext } from 'context/GlobalContext';
+import { Input } from 'components/ui/input';
+import { Button } from 'components/ui/button';
 
 function MenuBar() {
     const {dark, setDark, soundBoard, setSoundBoard} = useContext(GlobalContext);
+    const [isAddOpen, setIsAddOpen] = useState(false);
 
     return (
         <Menubar className="border shadow flex justify-between">
@@ -21,8 +25,9 @@ function MenuBar() {
                 <MenubarMenu>
                     <MenubarTrigger className="relative rounded">Group</MenubarTrigger>
                     <MenubarContent>
-                        <MenubarItem>Add Group</MenubarItem>
+                        <MenubarItem onClick={() => setIsAddOpen(true)}>Add Group</MenubarItem>
                     </MenubarContent>
+                    <AddGroupDialog isAddOpen={isAddOpen} setIsAddOpen={setIsAddOpen}/>
                 </MenubarMenu>
             </div>
 
@@ -42,6 +47,59 @@ function MenuBar() {
         </Menubar>
     );
 }
+
+function AddGroupDialog({ isAddOpen, setIsAddOpen }) {
+    const {soundBoard, setSoundBoard} = useContext(GlobalContext);
+    const [userInput, setUserInput] = useState("");
+
+    const handleAddGroup = (e) => {
+        e.preventDefault()
+        const newGroup = {
+            name: userInput.trim(),
+            sounds: [
+                // { name: "Cheer", path: "", volume: 70, muted: false }
+            ]
+        };
+        setSoundBoard(prevSoundBoard => [...prevSoundBoard, newGroup]);
+        setUserInput("")
+        setIsAddOpen(false)
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            handleAddGroup(e);
+        }
+    };
+
+    return (
+        <Dialog open={isAddOpen} onOpenChange={setIsAddOpen}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Enter Group Name</DialogTitle>
+                    <DialogDescription>
+                        Please provide the requested information below.
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="flex">
+                    <Input
+                    id="user-input"
+                    value={userInput}
+                    onChange={(e) => setUserInput(e.target.value)}
+                    className="col-span-3"
+                    onKeyDown={handleKeyDown}
+                    />
+                </div>
+                <DialogFooter>
+                    <Button variant="outline" onClick={() => setIsAddOpen(false)}>
+                        Cancel
+                    </Button>
+                    <Button type="submit" onClick={handleAddGroup}>Add</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
+
 
 export default MenuBar;
   
