c02c534ca30dd139938b61dd868d28fdbe15bae8
Jhone Jhee Patacsil <jhonejheeadrian.patacsil@g.batstate-u.edu.ph>
2024-11-21 02:13:06 +0000

    added master control card

--- src/layout/Board.jsx
+++ src/layout/Board.jsx
@@ -4,6 +4,7 @@ import { ToggleGroup, ToggleGroupItem } from "components/ui/toggle-group"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "components/ui/card"
 import { Slider } from "components/ui/slider"
 import { Volume1, Volume2, VolumeOff, VolumeX } from 'lucide-react'
+import { Button } from 'components/ui/button';
 
 
 function Board() {
@@ -32,6 +33,34 @@ function Board() {
 
     return (
         <div className='w-full h-full flex gap-2 items-start justify-start'>
+            {/* Global Sound Settings */}
+            <Card className="select-none shadow">
+                    
+                <CardHeader className="px-2 py-2">
+                    <CardTitle className="text-lg truncate">MASTER</CardTitle>
+                </CardHeader>
+
+                <CardContent className="p-2">
+                    <div className="flex flex-col gap-4 items-center justify-center">
+                        <Button variant="outline" size="icon" className="rounded-full">
+                            <VolumeX className='h-5 w-5'/>
+                        </Button>
+                        <Slider
+                            orientation="vertical"
+                            defaultValue={[50]}
+                            // value={[sound.volume * 100]}
+                            // onValueChange={(value) => handleVolumeChange(index, sindex, value)}
+                            max={100}
+                            min={0}
+                            step={1}
+                            // className={`${sound.muted ? "opacity-30" : "opacity-100"}`}
+                            thumbClassName="h-4 w-4 focus:outline-none focus-visible:ring-0 focus-visible:outline-none ring-offset-black"
+                            // disabled={sound.muted}
+                        />
+                    </div>
+                </CardContent>
+            </Card>
+
             {soundBoard.map((group, index) => (
                 <Card key={index} className="select-none shadow">
                     
@@ -45,6 +74,7 @@ function Board() {
                             className="w-fit flex items-center gap-2"
                             value={playlist}
                             onValueChange={(value) => setPlaylist(value)}
+                            variant="outline"
                         >
                             {group?.sounds.map((sound, sindex) => (
                                 <div key={sindex} className="flex flex-col gap-2">
@@ -52,7 +82,7 @@ function Board() {
                                     <ToggleGroupItem
                                         value={sound.name}
                                         className={`data-[state=on]:bg-green-500 data-[state=on]:text-green-50
-                                        hover:bg-white hover:text-black border min-w-[100px] max-w-[100px] min-h-[60px] max-h-[60px] shadow`}
+                                        hover:bg-white hover:text-black min-w-[100px] max-w-[100px] min-h-[60px] max-h-[60px] shadow`}
                                     >
                                         {sound.name}
                                     </ToggleGroupItem>
