ea3901c5b238f4aebfbff61eef42743e93657bfc
Jhone Jhee Patacsil <jhonejheeadrian.patacsil@g.batstate-u.edu.ph>
2024-11-23 08:35:26 +0000

    removed logging

--- src/layout/Board.jsx
+++ src/layout/Board.jsx
@@ -119,7 +119,6 @@ function Board() {
                 return updatedInstances;
             });
         } else {
-            console.log(sound)
             // Play the audio
             const audio = new Audio(sound.path);
             audio.volume = sound.volume / 100;
