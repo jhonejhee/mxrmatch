5212022107a382c730539e6486304fb3b71e2a66
Jhone Jhee Patacsil <jhonejheeadrian.patacsil@g.batstate-u.edu.ph>
2024-11-23 07:21:41 +0000

    removed unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import MxrMatch from "layout/index.js";
 import { GlobalContext } from 'context/GlobalContext';
 
